4facebe7760298a823341bc81065cf29425fa94b
Seif Allah Tarhouni <tarhouni.sat@gmail.com>
2020-04-26 17:04:51 +0200

    Deploy to Heroku

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from app.app import create_app
 
+app = create_app('app.config.Development')
+
 if __name__ == '__main__':
-    app = create_app('app.config.Development')
     app.run(host=app.config['HOST'], port=app.config['PORT'])
